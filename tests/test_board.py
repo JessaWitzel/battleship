c71d771f5bb35839c8b079759d6515a3b1d40553
Wayne Witzel III <wayne@riotousliving.com>
2016-10-25 23:00:10 -0400

    added initial Board class and some tests

---
+++ tests/test_board.py
@@ -0,0 +1,49 @@
+import pytest
+from battleship.board import Board
+
+
+def test_collides_ns():
+    board = Board()
+    board[0][0] = 'C'
+    board[1][0] = 'C'
+
+    ship = {'size': 2}
+    assert board.collides(0, 0, ship, 'NS')
+    assert not board.collides(1, 0, ship, 'NS')
+
+
+def test_collides_ew():
+    board = Board()
+    board[2][2] = 'C'
+    board[2][3] = 'C'
+
+    ship = {'size': 3}
+    assert board.collides(0, 2, ship, 'EW')
+    assert not board.collides(4, 2, ship, 'EW')
+
+
+def test_place_ship_ns():
+    board = Board()
+    ship = {'letter': 'C', 'size': 2}
+
+    board.place_ship(0, 0, ship, 'NS')
+    assert board[0][0] == 'C'
+    assert board[1][0] == 'C'
+
+
+def test_place_ship_ew():
+    board = Board()
+    ship = {'letter': 'C', 'size': 2}
+
+    board.place_ship(0, 0, ship, 'EW')
+    assert board[0][0] == 'C'
+    assert board[0][1] == 'C'
+
+
+def test_place_ship_exc():
+    board = Board()
+    ship = {'letter': 'C', 'size': 2}
+
+    with pytest.raises(IndexError) as excinfo:
+        board.place_ship(0, 9, ship, 'NS')
+    assert 'ship too large' in str(excinfo.value)
