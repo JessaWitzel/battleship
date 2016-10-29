e13db80abcfdbc228a1fcd5329525f126498c5d5
Jessa Witzel <jessa@riotousliving.com>
2016-10-29 00:04:55 -0400

    resolve issue #2

--- battleship/board.py
+++ battleship/board.py
@@ -8,40 +8,34 @@ class Board(object):
     def __getitem__(self, i):
         return self.board[i]
 
+    def get_sequence(self, col, row, sequence_size, direction):
+        sequence_values = []
+        if direction == 'NS':
+            for _ in range(0, sequence_size): #style note: _ for unnec var name
+                sequence_values.append(self.board[row][col])
+                row += 1
+        elif direction == 'EW':
+            for _ in range(0, sequence_size):
+                sequence_values.append(self.board[row][col])
+                col += 1
+        else:
+            raise ValueError("Please use a valid direction: 'NS' or 'EW'.")
+        return sequence_values
+
     def place_ship(self, col, row, ship, orientation):
         if not self.collides(col, row, ship, orientation):
             if orientation == 'NS':
-                if row + ship['size'] > 9:
-                    raise IndexError('ship too large to start on selected row')
-
-                for ir, r in enumerate(self.board):
-                    if ir >= row and ir <= row + ship['size']:
-                        for ic, c in enumerate(r):
-                            if ic == col:
-                                self.board[ir][ic] = ship['letter']
+                for _ in range(0, ship['size']):
+                    self.board[row][col] = ship['letter']
+                    row += 1
             else:
-                for ir, r in enumerate(self.board):
-                    if ir == row:
-                        for ic, c in enumerate(r):
-                            if ic >= col and ic <= col + ship['size']:
-                                self.board[ir][ic] = ship['letter']
+                for _ in range(0, ship['size']):
+                    self.board[row][col] = ship['letter']
+                    col += 1
 
     def collides(self, col, row, ship, orientation):
-        if orientation == 'NS':
-            for ir, r in enumerate(self.board):
-                # are you on the starting row or any row that would be touched
-                # from a ship of this size?
-                if ir >= row and ir <= row + ship['size']:
-                    for ic, c in enumerate(r):
-                        # are we on the column?
-                        if ic == col:
-                            if self.board[ir][ic] != 'S':
-                                return True
+        sequence_values = self.get_sequence(col, row, ship['size'], orientation)
+        if 'C' in sequence_values:
+            return True
         else:
-            for ir, r in enumerate(self.board):
-                if ir == row:
-                    for ic, c in enumerate(r):
-                        if ic >= col and ic <= col + ship['size']:
-                            if self.board[ir][ic] != 'S':
-                                return True
-        return False
+            return False
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,6 +1,13 @@
 import pytest
 from battleship.board import Board
 
+def test_get_sequence():
+    board = Board()
+    board[0][0] = 'C'
+    board[1][0] = 'C'
+
+    assert board.get_sequence(0, 0, 3, 'NS') == ['C', 'C', 'S']
+    assert board.get_sequence(0, 0, 2, 'EW') == ['C', 'S']
 
 def test_collides_ns():
     board = Board()
@@ -46,4 +53,4 @@ def test_place_ship_exc():
 
     with pytest.raises(IndexError) as excinfo:
         board.place_ship(0, 9, ship, 'NS')
-    assert 'ship too large' in str(excinfo.value)
+    assert 'index out of range' in str(excinfo.value)
