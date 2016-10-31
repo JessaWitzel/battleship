acaea155ab9b1e9caa2445685e456f1a73bc50ba
Wayne Witzel III <wayne@riotousliving.com>
2016-10-30 21:56:41 -0400

    move SHIPS to ships.py

--- battleship/board.py
+++ battleship/board.py
@@ -1,12 +1,5 @@
 DIRECTIONS = ('NS', 'EW')
 OCEAN = 'O'
-SHIPS = [
-    {'type':'carrier','size': 5, 'code': 'C'},
-    {'type': 'battleship', 'size': 4, 'code': 'B'},
-    {'type': 'cruiser', 'size': 3, 'code': 'R'},
-    {'type': 'submarine', 'size': 3, 'code': 'U'},
-    {'type': 'destroyer', 'size': 2, 'code': 'D'},
-]
 
 class Board(object):
     def __init__(self):
---
+++ battleship/ships.py
@@ -0,0 +1,7 @@
+SHIPS = [
+    {'type': 'carrier', 'size': 5, 'code': 'C'},
+    {'type': 'battleship', 'size': 4, 'code': 'B'},
+    {'type': 'cruiser', 'size': 3, 'code': 'R'},
+    {'type': 'submarine', 'size': 3, 'code': 'U'},
+    {'type': 'destroyer', 'size': 2, 'code': 'D'},
+]
